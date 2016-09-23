3e7638e99c1da05d4ff451437b8b0eb61984e777
lijiee <1028188271@qq.com>
2016-09-23 17:29:06 +0800

    练习

---
+++ ReactiveCocoaE/WKWebViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKWebViewViewController.h
+//  ReactiveCocoaE
+//
+//  Created by 李杰 on 16/9/23.
+//  Copyright © 2016年 李杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKWebViewViewController : UIViewController
+
+@end
---
+++ ReactiveCocoaE/WKWebViewViewController.m
@@ -0,0 +1,158 @@
+//
+//  WKWebViewViewController.m
+//  ReactiveCocoaE
+//
+//  Created by 李杰 on 16/9/23.
+//  Copyright © 2016年 李杰. All rights reserved.
+//
+#import <WebKit/WebKit.h>
+#import "WKWebViewViewController.h"
+
+@interface WKWebViewViewController ()<WKUIDelegate,WKNavigationDelegate>
+@property (nonatomic,strong)WKWebView *webView;
+
+@end
+
+@implementation WKWebViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.webView = [[WKWebView alloc]initWithFrame:self.view.bounds];
+    self.webView.UIDelegate = self;
+    self.webView.navigationDelegate = self;
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://share.ishenzang.com:1234/h5/ShowLectureNotification/lw123"]];
+    [self.webView loadRequest:request];
+    [self.view addSubview:self.webView];
+    
+    
+}
+/// 接收到服务器跳转请求之后调用 (服务器端redirect)，不一定调用
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation{
+    NSLog(@"--%@",navigation);
+    
+}
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    
+    // 如果响应的地址是百度，则允许跳转
+    if ([navigationResponse.response.URL.host.lowercaseString isEqual:@"www.baidu.com"]) {
+        
+        // 允许跳转
+        decisionHandler(WKNavigationResponsePolicyCancel);
+        return;
+    }
+    // 不允许跳转
+    decisionHandler(WKNavigationResponsePolicyAllow);
+}
+
+/**
+ *  在发送请求之前，决定是否跳转
+ *
+ *  @param webView          实现该代理的webview
+ *  @param navigationAction 当前navigation
+ *  @param decisionHandler  是否调转block
+ */
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+    // 如果请求的是百度地址，则延迟5s以后跳转
+    if ([navigationAction.request.URL.host.lowercaseString isEqual:@"www.baidu.com"]) {
+        
+        //        // 延迟5s之后跳转
+        //        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        //
+        //            // 允许跳转
+        //            decisionHandler(WKNavigationActionPolicyAllow);
+        //        });
+        
+        // 允许跳转
+        decisionHandler(WKNavigationActionPolicyCancel);
+        return;
+    }
+    // 不允许跳转
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+
+/// 2 页面开始加载
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
+    NSLog(@"--%@",navigation);
+
+}
+/// 4 开始获取到网页内容时返回
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
+}
+/// 5 页面加载完成之后调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    NSLog(@"--%@",navigation);
+
+}
+/// 页面加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+    NSLog(@"--%@",navigation);
+
+}
+
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)())completionHandler
+{
+    NSString *hostString = webView.URL.host;
+    NSString *sender = [NSString stringWithFormat:@"%@表示", hostString];
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:message message:sender preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"哦" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        completionHandler();
+    }]];
+    [self presentViewController:alertController animated:YES completion:^{}];
+}
+
+- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString *))completionHandler
+{
+    NSString *hostString = webView.URL.host;
+    NSString *sender = [NSString stringWithFormat:@"%@表示", hostString];
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:prompt message:sender preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        //textField.placeholder = defaultText;
+        textField.text = defaultText;
+    }];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        NSString *input = ((UITextField *)alertController.textFields.firstObject).text;
+        completionHandler(input);
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"提示" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        completionHandler(nil);
+    }]];
+    [self presentViewController:alertController animated:YES completion:^{}];
+}
+
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL))completionHandler
+{
+    NSString *hostString = webView.URL.host;
+    NSString *sender = [NSString stringWithFormat:@"%@表示", hostString];
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:message message:sender preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        completionHandler(YES);
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"即使" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        completionHandler(NO);
+    }]];
+    [self presentViewController:alertController animated:YES completion:^{}];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
